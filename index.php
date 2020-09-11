143ddfdc06f83174599e682fb929f5ec88a3b96a
rajmantra <71027265+rajmantra@users.noreply.github.com>
2020-09-11 13:29:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+
+        function myTest(){
+            static $x=0;
+            echo $x ;
+            $x++;
+        }
+        myTest();
+        echo "<br>";
+        myTest();
+        echo "<br>";
+        myTest();
+        
+        ?>
+    </body>
+</html>
